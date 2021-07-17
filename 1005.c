b716c6ef1351e4d072d671476b55198e7f87ecd7
ajwad-abrar <ajwadabrar@iut-dhaka.edu>
2021-07-18 01:19:42 +0600

    Added 1005.c

---
+++ 1005.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    float A,B,MEDIA;
+
+    scanf("%f%f", &A, &B);
+
+    MEDIA = ((3.5*A) + (7.5*B))/ 11;
+
+    if(A >= 0 && A <= 10 && B >= 0 && B <= 10){
+
+        printf("MEDIA = %.5f\n",MEDIA);
+    }
+
+    return 0;
+}
+
